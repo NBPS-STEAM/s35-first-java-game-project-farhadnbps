d6c869d59d7f275db1f8b977cffad5d419d69842
Farhad Ibrahimzade <67722444+farhadnbps@users.noreply.github.com>
2020-12-10 11:46:17 -0500

    gg

---
+++ App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class App {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String name = input.nextLine();
+        System.out.println(name);
+    }
+}
