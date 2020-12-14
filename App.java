e2b81b9baae4f0b703751f2eaddc81572ccf7ed9
Farhad Ibrahimzade <67722444+farhadnbps@users.noreply.github.com>
2020-12-14 11:49:35 -0500

    gg

--- App.java
+++ App.java
@@ -17,5 +17,26 @@ public class App {
         System.out.println("What is your name?");
         String name = input.nextLine();
         System.out.println("Let's start the game, " + name + "!");
+        System.out.println("Please enter game difficulty (easy/medium/hard):");
+        String difficulty = input.nextLine();
+        GetDifficulty(difficulty);
+    }
+
+    public static void GetDifficulty(String diff) {
+        boolean x = true;
+        while(x==true){
+            if(diff.equals("easy")){ // easy level questions
+                System.out.println("Let's begin with simple questions:");
+                x = false;
+            } 
+            else if(diff.equals("medium")){ // medium level questions
+                System.out.println("Let's try the medium questions now:");
+                x= false;
+            }
+            else if(diff.equals("hard")){ // hard level questions
+                System.out.println("Let's try the hardest questions!");
+                x = false;
+            } 
+            else System.out.println("Please enter a valid difficulty (easy/medium/hard) :");}
     }
 }
