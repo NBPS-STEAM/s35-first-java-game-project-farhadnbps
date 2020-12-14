076756e93b08e4ca11c2fc904975e6c200fe96be
Farhad Ibrahimzade <67722444+farhadnbps@users.noreply.github.com>
2020-12-14 11:59:59 -0500

    gg

--- App.java
+++ App.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 public class App {
     static Scanner input = new Scanner(System.in);
+    static String name = "";
 
     public static void main(String[] args) throws InterruptedException {
+        StartGame();
+        String difficulty = input.nextLine();
+        GetDifficulty(difficulty);
+    }
+
+    public static void StartGame() throws InterruptedException {
         System.out.println("Loading...10% complete");
         Thread.sleep(500);
         System.out.println("Loading...50% complete");
@@ -15,26 +22,25 @@ public class App {
         System.out.println("Loading...100% complete");
         Thread.sleep(500);
         System.out.println("What is your name?");
-        String name = input.nextLine();
+        name = input.nextLine();
         System.out.println("Let's start the game, " + name + "!");
         System.out.println("Please enter game difficulty (easy/medium/hard):");
-        String difficulty = input.nextLine();
-        GetDifficulty(difficulty);
+        
+        
     }
-
     public static void GetDifficulty(String diff) {
         boolean x = true;
         while(x==true){
             if(diff.equals("easy")){ // easy level questions
-                System.out.println("Let's begin with simple questions:");
+                System.out.println("Let's begin with simple questions, " + name + ":");
                 x = false;
             } 
             else if(diff.equals("medium")){ // medium level questions
-                System.out.println("Let's try the medium questions now:");
+                System.out.println("Let's try the medium questions now, " + name + ":");
                 x= false;
             }
             else if(diff.equals("hard")){ // hard level questions
-                System.out.println("Let's try the hardest questions!");
+                System.out.println("Let's try the hardest questions, " + name + "!");
                 x = false;
             } 
             else System.out.println("Please enter a valid difficulty (easy/medium/hard) :");}
